61203b2809c3b30ffd6ca91ed87ebc6d6b856992
Nicholas Cho <nickcho2@gmail.com>
2013-05-09 19:43:58 -0700

    Add EvolutionaryMusic class, and clean up gitignore.

---
+++ src/evolmusic/EvolutionaryMusic.java
@@ -0,0 +1,35 @@
+package evolmusic;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * @author Nicholas Cho
+ */
+public class EvolutionaryMusic {
+
+
+    /**
+     * Main method that checks commandline parameters and then runs the program
+     * with either default or given values.
+     *
+     * @param args Commandline arguments. Use "-i" to run interactively.
+     */
+    public static void main(String[] args) {
+        final int POPULATION_SIZE = 100;
+        final int NUMBER_MEASURES = 2;
+        final int BEATS_PER_MEASURE = 4;
+
+        // Populate our population with random melodies.
+        String[] population = new String[POPULATION_SIZE];
+        RandomMelody randomGenerator = new RandomMelody(
+                NUMBER_MEASURES,
+                BEATS_PER_MEASURE
+        );
+        for (int i = 0; i < population.length; i++) {
+            population[i] = randomGenerator.getMelodyString();
+        }
+
+        // TODO: rest of program
+    }
+}
